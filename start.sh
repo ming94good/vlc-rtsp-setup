6c7c74ad8b042ce9cfcaf36bf37c52fb285cd674
Ming <harry21105@gmail.com>
2020-09-25 00:49:25 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,10 @@
 #
 
 cd ~
-gnome-terminal -- ./start-cam1.sh
-gnome-terminal -- ./start-cam2.sh
 sleep 5
+gnome-terminal -- ./start-cam1.sh
+sleep 3
+gnome-terminal -- ./start-cam2.sh
+sleep 10
 gnome-terminal -- ./adjust-window.sh
 
